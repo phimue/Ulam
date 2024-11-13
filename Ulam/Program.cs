e2663be69e3b3044e2249c3ea3ac224014afcd07
phimue <79983949+phimue@users.noreply.github.com>
2024-11-13 23:33:57 +0100

    Added whole Ulam project

---
+++ Ulam/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ulam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] startValues = new int[] { 15, 31, 409599, 409600 };
+            
+            var ulamCalculator = new UlamCalculator();
+            ulamCalculator.RunUlam(startValues);
+
+            Console.Read();
+        }
+
+        
+    }
+}
